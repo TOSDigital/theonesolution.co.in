74141b89bb2d817bb5823c747999f514e9834e5f
TOSDigital <110897449+TOSDigital@users.noreply.github.com>
2023-01-29 18:20:53 +0530

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("navLinks");
+        function openMenu(){
+            navLinks.style.right = "0px";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-220px";
+        }
